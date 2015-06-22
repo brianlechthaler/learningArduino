ec019fe33c7a4d0ff67dfe8e46f89157cf16e678
Brian Lechthaler <brianlechthaler@gmail.com>
2015-06-22 14:47:31 -0700

    Revert "?"
    This reverts commit 3c4b60ee8bc90d538be785cfd3936af5bbad84c9.

--- sketch_jun22b/sketch_jun22b.ino
+++ sketch_jun22b/sketch_jun22b.ino
@@ -8,6 +8,8 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
+  // Print a message to the LCD.
+  //lcd.print("F**ks given:");
   
   randomSeed(analogRead(0));
 }
