7a4ac0cd9f744e8004a85d13bd6ce140e8bfd374
Brian Lechthaler <brianlechthaler@gmail.com>
2015-06-24 13:50:14 -0700

    remove some comments

--- serial_servo/serial_servo.ino
+++ serial_servo/serial_servo.ino
@@ -1,23 +1,24 @@
 #include <LiquidCrystal.h>
 #include <Servo.h>
 Servo myservo;
-int incomingByte = 0;   // for incoming serial data
-
+int incomingByte = 0;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {
-        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+        Serial.begin(9600);     
         myservo.attach(9);
         lcd.begin(16, 2);
 }
 
 void loop() {
 
-        // send data only when you receive data:
+        
         if (Serial.available() > 0) {
-          // read the incoming byte:
+
                 incomingByte = Serial.read();
                 myservo.write(incomingByte);
                 lcd.write(incomingByte);
+                delay(1000);
+                lcd.clear();
         }
 }
  
