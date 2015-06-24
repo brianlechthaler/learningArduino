a67662fa1f80736600e7a9f3210930a77e5090bd
Brian Lechthaler <brianlechthaler@gmail.com>
2015-06-24 11:01:45 -0700

    new app, serial_servo changes
    ^

---
+++ serial servo app/serial servo app/serial servo app/MainWindow.xaml.vb
@@ -0,0 +1,10 @@
+﻿Class MainWindow 
+
+End Class
+Sub SendSerialData(ByVal data As String)
+    ' Send strings to a serial port. 
+    Using com1 As IO.Ports.SerialPort =
+            My.Computer.Ports.OpenSerialPort("COM3")
+        com1.WriteLine("000")
+    End Using
+End Sub
--- serial_servo/serial_servo.ino
+++ serial_servo/serial_servo.ino
@@ -1,23 +1,23 @@
+#include <LiquidCrystal.h>
 #include <Servo.h>
 Servo myservo;
 int incomingByte = 0;   // for incoming serial data
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {
         Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
         myservo.attach(9);
+        lcd.begin(16, 2);
 }
 
 void loop() {
 
         // send data only when you receive data:
         if (Serial.available() > 0) {
-        ;
           // read the incoming byte:
                 incomingByte = Serial.read();
                 myservo.write(incomingByte);
-                // say what you got:
-                Serial.print("I received: ");
-                Serial.println(incomingByte, DEC);
+                lcd.write(incomingByte);
         }
 }
  
