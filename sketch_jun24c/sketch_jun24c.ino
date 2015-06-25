f9afaa0ea14b0f206157a6ef730749b261df7c8b
Brian Lechthaler <brianlechthaler@gmail.com>
2015-06-25 10:58:02 -0700

    the case of the missing .ino

---
+++ sketch_jun24c/sketch_jun24c.ino
@@ -0,0 +1,24 @@
+#include <LiquidCrystal.h>
+#include <Servo.h>
+Servo myservo;
+int incomingByte = 0;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup() {
+        Serial.begin(9600);     
+        myservo.attach(9);
+        lcd.begin(16, 2);
+}
+
+void loop() {
+
+        
+        if (Serial.available() > 0) {
+
+                incomingByte = Serial.read();
+                myservo.write(incomingByte);
+                lcd.write(incomingByte);
+                delay(1000);
+                lcd.clear();
+        }
+}
+ 
